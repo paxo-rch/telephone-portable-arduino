ce2da46e519a76f38476e83e97934e94712c07d2
paxo-rch <78375955+paxo-rch@users.noreply.github.com>
2021-03-09 17:04:16 +0100

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -13,34 +13,47 @@
 #include "SD.h"   
 
 /* library system */
+#include "hardwere.h"
 #include "Display.h"
 #include "Files.h"
+#include "background.h"
 #include "Utils.h"
 #include "Apps.h"
-#include "background.h"
+
 
 
 /* declarations & configurations */
 
+#define PIN_BUTTON_HOME 33
+
+void init_pin(void)
+{
+  pinMode(32, OUTPUT); digitalWrite(32, LOW); pinMode(PIN_BUTTON_HOME, INPUT_PULLUP); //bouton home
+  pinMode(12,OUTPUT);
+  
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
 
 void setup() {
   //affectation des broches
-  pinMode(32, OUTPUT); digitalWrite(32, LOW); pinMode(33, INPUT_PULLUP); //bouton home
-  pinMode(12,OUTPUT);
 
+  init_pin();
+  
   light(true);
+  
   tft.fillScreen(0x0000);
   init_bar();
   
   //demarages series
-  Serial.begin(9600);
-  Serial1.begin(9600);
   set_bar(30);
+  try_sd:
+  if (!SD.begin(53)){ error_bar(); error("carte sd non detectee"); while_not_press(); goto try_sd;}
+  set_bar(100);
   
-  if (!SD.begin(53)){ error_bar(); error("carte sd non detectee"); while(1);}
-  set_bar(60);
-
-  get_code();
+  while(!get_code());
+  
+  menu();
 }
 
 void loop() {
